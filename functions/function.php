9110e051f55bfaa9f12f95da6e381d93b76b704c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-09-28 15:02:02 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- functions/function.php
+++ functions/function.php
@@ -5,15 +5,15 @@
 
     session_start();
 
-    function add_user($user_name, $email, $mobile, $address){
+    function add_user($name_user, $email, $mobile, $address){
         $con = Connection();
 
-        echo $user_name;
+        echo $name_user;
         echo $email;
         echo $mobile;
         echo $address;
 
-        $insert = "INSERT INTO user_tbl(user_name,user_email,user_mobile,user_address,join_date)VALUES('$user_name','$email','$mobile','$address',NOW())";
+        $insert = "INSERT INTO user_tbl(user_name,user_email,user_mobile,user_address,join_date)VALUES('$name_user','$email','$mobile','$address',NOW())";
         $result_insert = mysqli_query($con, $insert);
 
     }
