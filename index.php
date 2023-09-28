917e1d42c7c49f0c65894873fc967dcfbf762cfc
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-09-28 10:33:48 +0530

    UPdate
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Ishini Hathurusinghe <101358580+ishinihathurusinghe@users.noreply.github.com>

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
                     </div><br>
                     <div class="row">
                         <div class="col-lg-6 intext">Email Address: </div>
-                        <div class="col-lg-6"><input type="text" name="nameuser" id="" class="form-control" placeholder="Email Address" required></div>
+                        <div class="col-lg-6"><input type="email" name="nameuser" id="" class="form-control" placeholder="Email Address" required></div>
                     </div>
                 </form>
             </div>
